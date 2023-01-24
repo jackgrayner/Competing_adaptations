edf76f3c6288e610a7bb0016c8b32dd658b324ef
jackgrayner <105981282+jackgrayner@users.noreply.github.com>
2023-01-24 18:39:49 +0000

    Update gen_filter_vcf.sh

--- gen_filter_vcf.sh
+++ gen_filter_vcf.sh
@@ -23,10 +23,9 @@ rm $VCF1.auto.vcf
 
 #filter SNP calls relative to mean sequencing depth (assuming format is "GT:DP:AD1,AD2:GQ:GL")
 Rscript filter_meanDepth_sex.R polarised.$VCF1.vcf polarised.$VCF1.depthfilt.vcf1
-cat vcfheader polarised.$VCF1.depthfilt.vcf1 > polarised.$VCF1.depthfilt.vcf
+cat vcfheader polarised.$VCF1.depthfilt.vcf1 | grep "^#\|0/1\|1/1" > polarised.$VCF1.depthfilt.vcf
 rm polarised.$VCF1.depthfilt.vcf1
-
 
 plink --vcf polarised.$VCF1.depthfilt.vcf --make-bed --allow-extra-chr --out $VCF.final 
 cp plink_correctedsex.fam ./$VCF.final.fam
-plink --bfile $VCF.final --assoc --allow-extra-chr --adjust --out CwPop_gwas 
+plink --bfile $VCF.final --mind 0.6 --geno 0.3 --assoc --allow-extra-chr --adjust --out CwPop_gwas 
