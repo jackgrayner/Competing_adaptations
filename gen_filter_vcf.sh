f65965e9aad9f407f4c3258a0ccd7dc1ff74f055
jackgrayner <105981282+jackgrayner@users.noreply.github.com>
2023-01-25 10:43:26 +0000

    Update gen_filter_vcf.sh

--- gen_filter_vcf.sh
+++ gen_filter_vcf.sh
@@ -26,6 +26,11 @@ Rscript filter_meanDepth_sex.R polarised.$VCF1.vcf polarised.$VCF1.depthfilt.vcf
 cat vcfheader polarised.$VCF1.depthfilt.vcf1 | grep "^#\|0/1\|1/1" > polarised.$VCF1.depthfilt.vcf
 rm polarised.$VCF1.depthfilt.vcf1
 
+#finally correct heterozygote calls using allelic balance (Meier script https://github.com/joanam/scripts/blob/master/allelicBalance.py)
+source activate python2
+python allelicBalance.py -i polarised.$VCF1.depthfilt.vcf -hom -o polarised.$VCF1.depthfilt.vcf1
+mv polarised.$VCF1.depthfilt.vcf1 ./polarised.$VCF1.depthfilt.vcf
+
 plink --vcf polarised.$VCF1.depthfilt.vcf --make-bed --allow-extra-chr --out $VCF.final 
 cp plink_correctedsex.fam ./$VCF.final.fam
 plink --bfile $VCF.final --mind 0.6 --geno 0.3 --assoc fisher --allow-extra-chr --adjust --out CwPop_gwas 
