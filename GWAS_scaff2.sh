07fffe74106545ecaf7eb5b5e20aac9c51a9a323
jackgrayner <105981282+jackgrayner@users.noreply.github.com>
2023-10-11 13:44:37 -0400

    Update and rename GWAS.sh to GWAS_scaff2.sh

--- GWAS_scaff2.sh
+++ GWAS_scaff2.sh
@@ -23,3 +23,4 @@ plink --allow-extra-chr --vcf $vcf.recode_anno.vcf.gz \
 --chr Scaffold_2 --double-id --remove remove.txt --thin 0.001 --make-bed --snps-only --geno 0.5 --allow-no-sex \
 --out $vcf.thin --export HV
 java -jar ~/scratch/apps/Haploview.jar -memory 5000
+
