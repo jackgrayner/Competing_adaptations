b278b4efdd32c494668111ed6ad83b2917b9cd4c
jackgrayner <105981282+jackgrayner@users.noreply.github.com>
2024-06-26 10:00:29 -0400

    Update GWAS_scaff1.sh

--- wgrs/GWAS_scaff1.sh
+++ wgrs/GWAS_scaff1.sh
@@ -1,11 +1,4 @@
-#!/bin/bash
-#SBATCH --job-name=bcftools_call
-#SBATCH --export=ALL
-#SBATCH --mem=16G
-#SBATCH --ntasks=4
-#SBATCH --nodes=1
-#SBATCH --partition=long
-#SBATCH --output=bcftools_call.out
+```sh
 vcf=$1#provide name of vcf
 ref=~/scratch/Cricket.curated.scaff_v2.fasta
 bamlist=./bamlist.txt#list of bam files
@@ -13,14 +6,16 @@ bcftools mpileup -Ov -f $ref -d 100000 --annotate FORMAT/AD,FORMAT/DP -b $bamlis
 vcftools --gzvcf $vcf.vcf.gz --maf 0.1 --minDP 5 --maxDP 70 --minQ 20 --minGQ 20 --recode --recode-INFO-all --out $vcf
 bgzip $vcf.recode.vcf
 bcftools annotate --set-id '%CHROM:%POS:%REF:%ALT' $vcf.recode.vcf.gz -O z -o $vcf.recode_anno.vcf.gz
+```
 
-#LD
+```sh
 plink --allow-extra-chr --vcf $vcf.recode_anno.vcf.gz \
 --chr Scaffold_1 --double-id --remove remove.txt --thin 0.001 --make-bed --snps-only --geno 0.5 --allow-no-sex \
 --out $vcf.thin --export HV
 java -jar ~/scratch/apps/Haploview.jar -memory 5000
+```
 
-#gemma assocation test
+```sh
 cp ./fw.fam $vcf.fam
 gemma -lm 2 -miss 0.15 -bfile $vcf -o $vcf
-
+```
